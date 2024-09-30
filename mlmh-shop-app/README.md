b7ffd429ce817133bf3b994e0687e1b73e77a683
Titoongh <lelong.work@gmail.com>
2024-09-30 16:28:05 +0200

    databse ready

--- mlmh-shop-app/README.md
+++ mlmh-shop-app/README.md
@@ -32,3 +32,12 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+# Next steps
+
+- [ ] Test all endpoints
+- [ ] Test prisma studio to see if my father could use it as it is otherwise create ugly forms to do it
+- [ ] fill database with 2 artists and 2 tabs per artist
+- [ ] Implement payment with stripe and/or paypal and test it
+- [ ] continue with implementing frontend features especially the search, product page, and payment page
+- [ ] Later: We will need to create custom links for downloading tabs: I think that I should create forms so that my dad can add its previous tabs and artists. When adding a dropbox link, my server should download the file directly and store it locally so that it can generate a one time download link.
---
+++ mlmh-shop-app/scrips/PopulateDatabase.ts
@@ -0,0 +1,58 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({ log: ['query'] })
+
+const main = async () => {
+    await prisma.artist.deleteMany()
+    await prisma.tablature.deleteMany()
+    await prisma.musicalGenre.deleteMany()
+    await prisma.artist.create({
+        data: {
+            name: 'Doc Watson',
+            musicalGenres: {
+                create: [
+                    {
+                        name: 'Bluegrass',
+                    },
+                ],
+            },
+            description: 'Doc Watson is really cool',
+            picture:
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Doc_Watson_Sugar_Grove.jpg/440px-Doc_Watson_Sugar_Grove.jpg',
+            tablatures: {
+                create: [
+                    {
+                        title: 'Deep River Blues',
+                        downloadLink:
+                            'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Doc_Watson_Sugar_Grove.jpg/440px-Doc_Watson_Sugar_Grove.jpg',
+                        description: 'Doc Watson playing Deep River Blues',
+                        musicalGenres: {
+                            create: [
+                                {
+                                    name: 'Blues',
+                                },
+                            ],
+                        },
+                        contents: {
+                            create: [
+                                {
+                                    type: 'IMAGE',
+                                    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Doc_Watson_Sugar_Grove.jpg/440px-Doc_Watson_Sugar_Grove.jpg',
+                                    rank: 1,
+                                },
+                            ],
+                        },
+                    },
+                ],
+            },
+        },
+    })
+}
+
+main()
+    .then(() => {
+        console.log('Database populated')
+    })
+    .catch(e => {
+        console.error(e)
+    })
