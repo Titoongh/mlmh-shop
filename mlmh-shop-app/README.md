37ceb0db0385131a2e29c780a11f085d99acb65f
Titoongh <lelong.work@gmail.com>
2024-10-03 19:06:57 +0200

    retrieve product info on server for download

--- mlmh-shop-app/README.md
+++ mlmh-shop-app/README.md
@@ -13,7 +13,7 @@ docker compose up --build
 - [x] Test all endpoints
 - [x] Test prisma studio to see if my father could use it as it is otherwise create ugly forms to do it
 - [x] fill database with 2 artists and 2 tabs per artist
-- [ ] Implement payment with stripe and/or paypal and test it
+- [x] Implement payment with stripe and/or paypal and test it
 - [ ] continue with implementing frontend features especially the search, product page, and payment page
 - [ ] Later: We will need to create custom links for downloading tabs: I think that I should create forms so that my dad can add its previous tabs and artists. When adding a dropbox link, my server should download the file directly and store it locally so that it can generate a one time download link.
 
--- mlmh-shop-app/app/checkout/page.tsx
+++ mlmh-shop-app/app/checkout/page.tsx
@@ -8,11 +8,20 @@ const stripePromise = loadStripe(
 
 export default function PreviewPage() {
     const [isLoading, setIsLoading] = useState(false)
+    const [isSuccess, setIsSuccess] = useState(false)
 
     const tablatureIds = [
         'aee0a5a5-f93f-467b-8bf0-165247e648d7',
         'e482f226-c0c8-4d98-88fa-3990bc229e71',
     ]
+
+    useEffect(() => {
+        // if success=true in the URL, set isSuccess to true
+        const isSuccess = new URLSearchParams(window.location.search).get(
+            'success',
+        )
+        if (isSuccess === 'true') setIsSuccess(true)
+    }, [setIsSuccess])
 
     const handleCheckout = async () => {
         setIsLoading(true)
@@ -39,10 +48,52 @@ export default function PreviewPage() {
         }
     }
 
+    const handleDownload = async () => {
+        setIsLoading(true)
+        try {
+            const sessionId = new URLSearchParams(window.location.search).get(
+                'session_id',
+            )
+            console.log('frontend sessionId', sessionId)
+            const response = await fetch(
+                `/api/download?session_id=${sessionId}`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                },
+            )
+
+            if (response.ok) {
+                const blob = await response.blob()
+                const url = URL.createObjectURL(blob)
+                const a = document.createElement('a')
+                a.href = url
+                a.download = 'tablature.jpg'
+                a.click()
+            } else {
+                // Handle errors
+                console.error('Download failed')
+            }
+        } catch (error) {
+            console.error('Error during download:', error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     return (
         <div>
-            <button onClick={handleCheckout} disabled={isLoading}>
-                {isLoading ? 'Processing...' : 'Checkout'}
+            <button
+                onClick={isSuccess ? handleDownload : handleCheckout}
+                disabled={isLoading}
+            >
+                {isLoading
+                    ? 'Processing...'
+                    : isSuccess
+                      ? 'Download'
+                      : 'Checkout'}
             </button>
         </div>
     )
