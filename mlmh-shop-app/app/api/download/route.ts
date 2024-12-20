c8b6e785d8b000da24ce7fcd8c44bc67f6792ac4
Titoongh <lelong.work@gmail.com>
2024-12-20 23:12:25 +0100

    add brevo mail after purchase

--- mlmh-shop-app/app/api/download/route.ts
+++ mlmh-shop-app/app/api/download/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
-import { promises as fs } from 'fs'
-import path from 'path'
 import { prisma } from '@/app/prisma'
 import JSZip from 'jszip'
 
