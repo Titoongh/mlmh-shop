5feed52620e5f6b09aa4e489e6d8ab28cd570f7e
Titoongh <lelong.work@gmail.com>
2025-02-02 23:28:54 +0100

    fix dynamic api routes...

--- mlmh-shop-app/app/api/musical-genres/route.ts
+++ mlmh-shop-app/app/api/musical-genres/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '@/app/prisma'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     const musicalGenres = await prisma.musicalGenre.findMany({
         include: {
--- mlmh-shop-app/app/api/tablatures/route.ts
+++ mlmh-shop-app/app/api/tablatures/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import { prisma } from '../../prisma'
 import { safeTablatureSelect } from './utils'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     const tablatures = await prisma.tablature.findMany({
         select: safeTablatureSelect,
