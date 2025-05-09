7c4c49c694e7f9fb93c80219771adc0ab4a79bf4
Titoongh <lelong.work@gmail.com>
2025-05-09 16:35:14 +0200

    update entrypoint

--- mlmh-shop-app/docker-entrypoint.sh
+++ mlmh-shop-app/docker-entrypoint.sh
@@ -18,25 +18,25 @@ if [ -f "/run/secrets/MLMH_CLERK_SECRET_KEY" ]; then
   export CLERK_SECRET_KEY=$(cat /run/secrets/MLMH_CLERK_SECRET_KEY)
 fi
 if [ -f "/run/secrets/MLMH_SCW_REGION" ]; then
-  export SCW_REGION=$(cat /run/secrets/SCW_REGION)
+  export SCW_REGION=$(cat /run/secrets/MLMH_SCW_REGION)
 fi
 if [ -f "/run/secrets/MLMH_SCW_BUCKET_ENDPOINT" ]; then
-  export SCW_BUCKET_ENDPOINT=$(cat /run/secrets/SCW_BUCKET_ENDPOINT)
+  export SCW_BUCKET_ENDPOINT=$(cat /run/secrets/MLMH_SCW_BUCKET_ENDPOINT)
 fi
 if [ -f "/run/secrets/MLMH_SCW_DEFAULT_ORGANIZATION_ID" ]; then
-  export SCW_DEFAULT_ORGANIZATION_ID=$(cat /run/secrets/SCW_DEFAULT_ORGANIZATION_ID)
+  export SCW_DEFAULT_ORGANIZATION_ID=$(cat /run/secrets/MLMH_SCW_DEFAULT_ORGANIZATION_ID)
 fi
 if [ -f "/run/secrets/MLMH_SCW_DEFAULT_PROJECT_ID" ]; then
-  export SCW_DEFAULT_PROJECT_ID=$(cat /run/secrets/SCW_DEFAULT_PROJECT_ID)
+  export SCW_DEFAULT_PROJECT_ID=$(cat /run/secrets/MLMH_SCW_DEFAULT_PROJECT_ID)
 fi
 if [ -f "/run/secrets/MLMH_SCW_ACCESS_KEY" ]; then
-  export SCW_ACCESS_KEY=$(cat /run/secrets/SCW_ACCESS_KEY)
+  export SCW_ACCESS_KEY=$(cat /run/secrets/MLMH_SCW_ACCESS_KEY)
 fi
 if [ -f "/run/secrets/MLMH_SCW_SECRET_KEY" ]; then
-  export SCW_SECRET_KEY=$(cat /run/secrets/SCW_SECRET_KEY)
+  export SCW_SECRET_KEY=$(cat /run/secrets/MLMH_SCW_SECRET_KEY)
 fi
 if [ -f "/run/secrets/MLMH_SCW_BUCKET_NAME" ]; then
-  export SCW_BUCKET_NAME=$(cat /run/secrets/SCW_BUCKET_NAME)
+  export SCW_BUCKET_NAME=$(cat /run/secrets/MLMH_SCW_BUCKET_NAME)
 fi
 
 # Generate Prisma client with --accelerate at runtime when we have the real DATABASE_URL
